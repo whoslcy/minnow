916ff6a5986fb4e23c01367bd9fb562e9576abd3
李辰宇 <shawnaigame@qq.com>
2025-03-10 21:35:03 +0800

    [check2] Implement TCPReceiver
    But I am still confused with my own implementation.

--- src/tcp_receiver.hh
+++ src/tcp_receiver.hh
@@ -1,20 +1,21 @@
 #pragma once
 
+#include "byte_stream.hh"
 #include "reassembler.hh"
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
 
-class TCPReceiver
-{
-public:
+class TCPReceiver {
+ public:
   // Construct with given Reassembler
-  explicit TCPReceiver( Reassembler&& reassembler ) : reassembler_( std::move( reassembler ) ) {}
+  explicit TCPReceiver(Reassembler&& reassembler)
+      : reassembler_(std::move(reassembler)) {}
 
   /*
-   * The TCPReceiver receives TCPSenderMessages, inserting their payload into the Reassembler
-   * at the correct stream index.
+   * The TCPReceiver receives TCPSenderMessages, inserting their payload into
+   * the Reassembler at the correct stream index.
    */
-  void receive( TCPSenderMessage message );
+  void receive(TCPSenderMessage message);
 
   // The TCPReceiver sends TCPReceiverMessages to the peer's TCPSender.
   TCPReceiverMessage send() const;
@@ -25,6 +26,14 @@ public:
   const Reader& reader() const { return reassembler_.reader(); }
   const Writer& writer() const { return reassembler_.writer(); }
 
-private:
+ private:
+  uint64_t AbsoluteSequenceNumberOfFirstUnassembled() const {
+    return reassembler_.writer().bytes_pushed() + 1;
+  }
+
+  bool IsSynReceived() const { return zero_point_.has_value(); }
+
+  bool is_finished_{false};
+  std::optional<Wrap32> zero_point_{std::nullopt};
   Reassembler reassembler_;
 };
